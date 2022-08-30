5c684d0518618a403aa20619305f649abbbb35c0
adushaj2022 <abcdushaj@gmail.com>
2022-08-30 17:52:12 -0400

    new route to add badge to participant

--- backend/api/meeting/controller.js
+++ backend/api/meeting/controller.js
@@ -16,6 +16,26 @@ module.exports = {
         await Meeting.findOneAndUpdate(query, req.body, {upsert: true},);
         return res.send({ status: true })
     },
+
+    async addBadgeToParticipant(req, res) {
+        const { meetingId, pid, badge } = req.body;
+
+        const meeting = await Meeting.findOne({ meetingId });
+        if (!meeting) {
+            res.send({error: 'meeting does not exist'});
+        } else {
+            meeting.participants = meeting.participants.map((p) => {
+                if (p.participantId == pid) {
+                    p.badges.push(badge)
+                }
+
+                return p;
+            })
+
+            await meeting.save();
+            res.send(meeting);
+        }
+    }
 }
 
 
--- backend/api/meeting/router.js
+++ backend/api/meeting/router.js
@@ -4,5 +4,6 @@ const controller = require('./controller')
 router
   .get('/:id', controller.getMeeting)
   .post('/save', controller.saveMeeting)
+  .post('/badge', controller.addBadgeToParticipant)
 
 module.exports = router
